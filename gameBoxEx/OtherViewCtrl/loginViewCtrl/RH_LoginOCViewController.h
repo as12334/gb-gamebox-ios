a58bc9e45dbf2715b63085e472a578f538b6143b
Lewis <lewisch777@gmail.com>
2017-10-30 15:08:30 +0800

    登录界面用OC原生代码进行了编写，另外设置了OC和H5的登录界面的开关

---
+++ gameBoxEx/OtherViewCtrl/loginViewCtrl/RH_LoginOCViewController.h
@@ -0,0 +1,24 @@
+//
+//  RH_LoginViewController.h
+//  TaskTracking
+//
+//  Created by apple pro on 2017/2/18.
+//  Copyright © 2017年 jinguihua. All rights reserved.
+//
+
+#import "RH_BasicViewController.h"
+
+@class RH_LoginOCViewController;
+@protocol RH_LoginOCViewControllerDelegate <NSObject>
+
+@optional
+- (void)userLoginViewControllerDidSucceedLogin:(RH_LoginOCViewController *)userLoginViewController;
+
+@end
+
+
+@interface RH_LoginOCViewController : RH_BasicViewController
++(instancetype)shareLoginViewController ;
+
+@property(nonatomic,weak) id<RH_LoginOCViewControllerDelegate> delegate;
+@end
