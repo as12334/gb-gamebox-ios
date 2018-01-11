ba96b6cdd65af3b97b950b72fba0c75f53bbaf93
Lenny <lenny@LennydeMacBook-Pro.local>
2018-01-11 16:49:35 +0800

    1.11.2

--- gameBoxEx/OtherViewCtrl/preferentialList/cells/RH_PreferentialListCell.h
+++ gameBoxEx/OtherViewCtrl/preferentialList/cells/RH_PreferentialListCell.h
@@ -8,14 +8,27 @@
 
 #import "CLTableViewCell.h"
 
+typedef enum : NSUInteger {
+    PreferentialListCellBackTypeNotGrant,
+    PreferentialListCellBackTypeDidGrant,
+    PreferentialListCellBackTypePermitting,
+} PreferentialListCellBackType;
+
+
 @interface RH_PreferentialListCell : CLTableViewCell
 
 @property (weak, nonatomic) IBOutlet UILabel *label_Title;
 
+@property (weak, nonatomic) IBOutlet UILabel *label_Bottom;
 
+@property (weak, nonatomic) IBOutlet UILabel *label_Time;
 
+@property (weak, nonatomic) IBOutlet UIImageView *image_Right;
 
+@property (weak, nonatomic) IBOutlet UILabel *label_RightMoney;
 
+@property (weak, nonatomic) IBOutlet UILabel *label_Grant;
 
+- (void)setCellGranted:(PreferentialListCellBackType )grant;
 
 @end
