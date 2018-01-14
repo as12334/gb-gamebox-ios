bd9048a9753248e2f995fb20670ece8bbe5f4834
Lenny <lenny@LennydeMacBook-Pro.local>
2018-01-14 20:31:25 +0800

    1.14.3

---
+++ gameBoxEx/OtherViewCtrl/withdrawMoney/cells/RH_WithdrawMoneyLowCell.h
@@ -0,0 +1,13 @@
+//
+//  RH_WithdrawMoneyLowCell.h
+//  gameBoxEx
+//
+//  Created by Lenny on 2018/1/14.
+//  Copyright © 2018年 luis. All rights reserved.
+//
+
+#import "CLTableViewCell.h"
+
+@interface RH_WithdrawMoneyLowCell : CLTableViewCell
+
+@end
