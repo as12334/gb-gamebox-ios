c51643e7730dab199ac42579cf8382699c1dd469
luis <luis@dawoo.com>
2018-01-30 17:20:07 +0800

    changed

--- gameBoxEx/OtherViewCtrl/gameList/RH_LotteryGameListTopView.m
+++ gameBoxEx/OtherViewCtrl/gameList/RH_LotteryGameListTopView.m
@@ -10,6 +10,7 @@
 #import "coreLib.h"
 
 @interface RH_LotteryGameListTopView()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIButton *button;
 
@@ -25,11 +26,11 @@
 -(void)awakeFromNib
 {
     [super awakeFromNib];
-    UIView *contentView = [UIView new];
-    [self addSubview:contentView];
-    contentView.whc_TopSpace(12).whc_LeftSpace(20).whc_RightSpace(20).whc_BottomSpace(5);
-    [contentView addSubview:self.textField];
-    [contentView addSubview:self.button];
+//    UIView *contentView = [UIView new];
+//    [self addSubview:contentView];
+    self.contentView.whc_TopSpace(12).whc_LeftSpace(20).whc_RightSpace(20).whc_BottomSpace(5);
+//    [contentView addSubview:self.textField];
+//    [contentView addSubview:self.button];
     self.button.whc_TopSpace(0).whc_RightSpace(0).whc_BottomSpace(0).whc_Width(60);
     self.textField.whc_TopSpace(0).whc_BottomSpace(0).whc_LeftSpace(0).whc_RightSpaceToView(0, self.button);
     self.textField.layer.borderColor = [[UIColor lightGrayColor]CGColor];
@@ -37,8 +38,11 @@
     self.textField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0,10, self.textField.frame.size.height)];
     self.textField.leftViewMode = UITextFieldViewModeAlways;
     self.textField.delegate = self;
-    contentView.layer.cornerRadius = 5;
-    contentView.clipsToBounds = YES;
+    self.contentView.layer.borderWidth = 1.0f ;
+    self.contentView.layer.borderColor = [UIColor redColor].CGColor ;
+    self.contentView.layer.cornerRadius = 5;
+    self.contentView.layer.masksToBounds = YES ;
+//    contentView.clipsToBounds = YES;
 }
 
 -(BOOL)isEdit
