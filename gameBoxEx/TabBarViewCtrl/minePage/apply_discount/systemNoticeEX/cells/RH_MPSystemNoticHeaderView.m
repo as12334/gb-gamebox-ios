241180399b9783a8627a5ddfc4c7b9ff409b13af
sam <sam@samdeMacBook-Pro-2.local>
2018-02-04 21:03:46 +0800

    changed

--- gameBoxEx/TabBarViewCtrl/minePage/apply_discount/systemNoticeEX/cells/RH_MPSystemNoticHeaderView.m
+++ gameBoxEx/TabBarViewCtrl/minePage/apply_discount/systemNoticeEX/cells/RH_MPSystemNoticHeaderView.m
@@ -39,7 +39,16 @@
     self.endDateView.layer.borderColor = colorWithRGB(226, 226, 226).CGColor;
     self.endDateView.layer.masksToBounds = YES;
     self.kuaixuanBtn.layer.cornerRadius = 3.f;
-    self.kuaixuanBtn.backgroundColor = colorWithRGB(27, 117, 217);
+    if ([THEMEV3 isEqualToString:@"green"]){
+        self.kuaixuanBtn.backgroundColor = RH_NavigationBar_BackgroundColor_Green;
+    }else if ([THEMEV3 isEqualToString:@"red"]){
+        self.kuaixuanBtn.backgroundColor = RH_NavigationBar_BackgroundColor_Red;
+    }else if ([THEMEV3 isEqualToString:@"black"]){
+        self.kuaixuanBtn.backgroundColor = RH_NavigationBar_BackgroundColor_Black;
+    }else{
+        self.kuaixuanBtn.backgroundColor = RH_NavigationBar_BackgroundColor;
+    }
+    
     [self.kuaixuanBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     self.kuaixuanBtn.layer.masksToBounds = YES;
     self.startSeletedDateView.dateLabel.textColor = colorWithRGB(153, 153, 153);
