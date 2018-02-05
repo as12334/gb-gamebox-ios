ba64890b56ee3e59b270335e55b41f96692af30d
luis <luis@dawoo.com>
2018-02-05 20:22:11 +0800

    changed

--- gameBoxEx/OtherViewCtrl/BettingRecord/RH_BettingRecordBottomView.m
+++ gameBoxEx/OtherViewCtrl/BettingRecord/RH_BettingRecordBottomView.m
@@ -83,6 +83,9 @@
         titleCell.titleFont = [UIFont systemFontOfSize:14.0f] ;
         titleCell.titleColor = colorWithRGB(85, 85, 85);
         titleCell.labTitle.textAlignment = NSTextAlignmentLeft ;
+        
+        //配制 layout
+        titleCell.labTitle.whc_LeftSpace(10).whc_CenterY(0).whc_WidthAuto() ;
     }
     
     NSInteger index = indexPath.section*2 + indexPath.item ;
