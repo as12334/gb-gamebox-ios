0d8cf9b849e4a739474594e35e98d1bff3f6ab90
Richard <richard@RicharddeMacBook-Pro.local>
2018-02-06 16:52:43 +0800

    changed

--- gameBoxEx/TabBarViewCtrl/minePage/apply_discount/systemNoticeEX/cells/RH_SystemNoticeListView.m
+++ gameBoxEx/TabBarViewCtrl/minePage/apply_discount/systemNoticeEX/cells/RH_SystemNoticeListView.m
@@ -62,7 +62,6 @@
     cell.textLabel.text = array[indexPath.row];
     cell.textLabel.font = [UIFont systemFontOfSize:12.f];
     cell.textLabel.textColor = colorWithRGB(153, 153, 153);
-   
     cell.textLabel.textAlignment = NSTextAlignmentCenter;
     return cell;
 }
