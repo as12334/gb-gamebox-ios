6c4afb2123b1e3abe4e92e0e9f97254248815e5e
lewis <lewis@dawoo.com>
2018-03-22 10:01:36 +0800

    changed

--- gameBoxEx/TabBarViewCtrl/depositePage/RH_DepositeViewControllerEX.m
+++ gameBoxEx/TabBarViewCtrl/depositePage/RH_DepositeViewControllerEX.m
@@ -26,13 +26,18 @@
     // Do any additional setup after loading the view.
     //增加login status changed notification
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleNotification:) name:NT_LoginStatusChangedNotification object:nil] ;
-    [self setNeedUpdateView];
-   
+    
+    [self setupUI];
 }
+-(void)viewWillAppear:(BOOL)animated
+{
+    
+}
+#pragma mark --检测是否登录
 -(void)handleNotification:(NSNotification*)nt
 {
     if ([nt.name isEqualToString:NT_LoginStatusChangedNotification]){
-        [self setNeedUpdateView] ;
+        [self updateView] ;
     }
 }
 -(void)updateView
@@ -48,23 +53,66 @@
         showAlertView(@"无网络", @"") ;
     }
 }
+#pragma mark --视图
+-(void)setupUI{
+    self.contentTableView = [self createTableViewWithStyle:UITableViewStyleGrouped updateControl:NO loadControl:NO] ;
+    self.contentTableView.delegate = self   ;
+    self.contentTableView.dataSource = self ;
+    self.contentTableView.sectionFooterHeight = 0.0f ;
+    self.contentTableView.sectionHeaderHeight = 0.0f ;
+    [self.contentView addSubview:self.contentTableView] ;
+}
+#pragma mark-tableView
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1 ;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+   
+    return  10 ;
+   
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return 50.0f ;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return self.loadingIndicateTableViewCell ;
+  
+}
+
+//-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    if (indexPath.section==1){
+//        RH_HomePageModel *homePageModel = ConvertToClassPointer(RH_HomePageModel, [self.pageLoadManager dataAtIndex:0]) ;
+//        if (self.rhAlertView.superview == nil) {
+//            self.rhAlertView = [RH_BasicAlertView createInstance];
+//            self.rhAlertView.alpha = 0;
+//            [self.view.window addSubview:self.rhAlertView];
+//            self.rhAlertView.whc_TopSpace(0).whc_LeftSpace(0).whc_BottomSpace(0).whc_RightSpace(0);
+//            
+//            [UIView animateWithDuration:0.3 animations:^{
+//                self.rhAlertView.alpha = 1;
+//            } completion:^(BOOL finished) {
+//                if (finished) {
+//                    [self.rhAlertView showContentWith:homePageModel.mAnnouncementList];
+//                    
+//                }
+//            }];
+//        }
+//    }
+//}
+
 - (void)dealloc
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self] ;
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
