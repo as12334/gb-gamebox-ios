779f9d3ae284303be504b46db2f3851fee6f90b1
Lenny <lenny@LennydeMacBook-Pro.local>
2018-03-22 19:52:33 +0800

    3.22.2

---
+++ gameBoxEx/OtherViewCtrl/loginViewCtrl/cell/RH_RegistrationViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RH_RegistrationViewCell.h
+//  gameBoxEx
+//
+//  Created by Lenny on 2018/3/22.
+//  Copyright © 2018年 luis. All rights reserved.
+//
+
+#import "CLTableViewCell.h"
+
+@interface RH_RegistrationViewCell : CLTableViewCell
+
+@end
---
+++ gameBoxEx/OtherViewCtrl/loginViewCtrl/cell/RH_RegistrationViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RH_RegistrationViewCell.m
+//  gameBoxEx
+//
+//  Created by Lenny on 2018/3/22.
+//  Copyright © 2018年 luis. All rights reserved.
+//
+
+#import "RH_RegistrationViewCell.h"
+
+@implementation RH_RegistrationViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
