0a238bd33a1c3f4c22b0a3096bd3eb3506199384
Richard <richard@RicharddeMacBook-Pro.local>
2018-03-22 10:00:39 +0800

    changed

--- gameBoxEx/TabBarViewCtrl/depositePage/RH_DepositeViewControllerEX.m
+++ gameBoxEx/TabBarViewCtrl/depositePage/RH_DepositeViewControllerEX.m
@@ -7,8 +7,9 @@
 //
 
 #import "RH_DepositeViewControllerEX.h"
+#import "RH_LoginViewControllerEx.h"
 
-@interface RH_DepositeViewControllerEX ()
+@interface RH_DepositeViewControllerEX ()<LoginViewControllerExDelegate>
 
 @end
 
@@ -21,18 +22,35 @@
 {
     return YES  ;
 }
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated] ;
+    if ([self needLogin]){
+        //check whether login
+        if (!self.appDelegate.isLogin){
+            if ([SITE_TYPE isEqualToString:@"integratedv3"] || [SITE_TYPE isEqualToString:@"integratedv3oc"]){
+                //push login viewController
+                RH_LoginViewControllerEx *loginViewCtrlEx = [RH_LoginViewControllerEx viewControllerWithContext:@(YES)];
+                loginViewCtrlEx.delegate = self ;
+                [self presentViewController:loginViewCtrlEx animated:YES completion:nil] ;
+            }
+        }
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     //增加login status changed notification
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleNotification:) name:NT_LoginStatusChangedNotification object:nil] ;
-    [self setNeedUpdateView];
+   
    
 }
 -(void)handleNotification:(NSNotification*)nt
 {
     if ([nt.name isEqualToString:NT_LoginStatusChangedNotification]){
-        [self setNeedUpdateView] ;
+        [self updateView] ;
     }
 }
 -(void)updateView
@@ -42,10 +60,60 @@
     }
     else if(!self.appDelegate.isLogin){
         //进入登录界面
-        [self loginButtonItemHandle] ;
+//        [self loginButtonItemHandle] ;
     }
     else if (NetNotReachability()){
         showAlertView(@"无网络", @"") ;
+    }
+}
+
+#pragma mark-
+-(void)loginViewViewControllerExTouchBack:(RH_LoginViewControllerEx *)loginViewContrller BackToFirstPage:(BOOL)bFirstPage
+{
+    if (loginViewContrller.presentingViewController){
+        [loginViewContrller dismissViewControllerAnimated:YES completion:nil] ;
+    }else{
+        [self.navigationController popViewControllerAnimated:YES] ;
+    }
+    
+    if (bFirstPage){
+        if ([SITE_TYPE isEqualToString:@"integratedv3"] || [SITE_TYPE isEqualToString:@"integratedv3oc"]){
+            [self.navigationController popToRootViewControllerAnimated:NO];
+            self.myTabBarController.selectedIndex = 2 ;
+        }else{
+            self.myTabBarController.selectedIndex = 0 ;
+        }
+    }
+}
+
+-(void)loginViewViewControllerExLoginSuccessful:(RH_LoginViewControllerEx *)loginViewContrller
+{
+    if (loginViewContrller.presentingViewController){
+        [loginViewContrller dismissViewControllerAnimated:YES completion:nil] ;
+    }else{
+        [self.navigationController popViewControllerAnimated:YES] ;
+    }
+    
+}
+
+-(void)loginViewViewControllerExSignSuccessful:(RH_LoginViewControllerEx *)loginViewContrller SignFlag:(BOOL)bFlag
+{
+    if (loginViewContrller.presentingViewController){
+        [loginViewContrller dismissViewControllerAnimated:YES completion:nil] ;
+    }else{
+        [self.navigationController popViewControllerAnimated:YES] ;
+    }
+
+    if (bFlag==false){
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *account = [defaults stringForKey:@"account"] ;
+        NSString *password = [defaults stringForKey:@"password"] ;
+        
+        if ([SITE_TYPE isEqualToString:@"integratedv3"] || [SITE_TYPE isEqualToString:@"integratedv3oc"]){
+            [self.serviceRequest startAutoLoginWithUserName:account Password:password] ;
+        }else{
+            [self.serviceRequest startLoginWithUserName:account Password:password VerifyCode:nil] ;
+        }
     }
 }
 - (void)dealloc
