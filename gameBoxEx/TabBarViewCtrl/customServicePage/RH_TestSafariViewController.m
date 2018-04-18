4efddb00032c285a21f21e7619ee960069d0afb3
sam <sam@samdeMacBook-Pro-2.local>
2018-04-18 17:24:21 +0800

    changed

--- gameBoxEx/TabBarViewCtrl/customServicePage/RH_TestSafariViewController.m
+++ gameBoxEx/TabBarViewCtrl/customServicePage/RH_TestSafariViewController.m
@@ -121,7 +121,12 @@
     //增加login status changed notification
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleNotification:) name:NT_LoginStatusChangedNotification object:nil] ;
     [self.webView setScalesPageToFit:NO];
-    self.webView.scrollView.contentInset = UIEdgeInsetsMake(20, 0,46, 0);
+    if (SID isEqualToString:@"119" || [SID isEqualToString:@"270"]) {
+        self.webView.scrollView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0);
+    }else{
+        self.webView.scrollView.contentInset = UIEdgeInsetsMake(20, 0, 64, 0);
+    }
+    
     self.webView.frame = self.view.frame;
  
 }
