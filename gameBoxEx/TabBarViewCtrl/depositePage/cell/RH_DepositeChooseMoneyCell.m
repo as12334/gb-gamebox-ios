254b0833bb9cf4a4840a7cc41414d5e5406deb37
shin <shin@dawoo.com>
2018-06-04 17:26:41 +0800

    xxx

--- gameBoxEx/TabBarViewCtrl/depositePage/cell/RH_DepositeChooseMoneyCell.m
+++ gameBoxEx/TabBarViewCtrl/depositePage/cell/RH_DepositeChooseMoneyCell.m
@@ -35,7 +35,7 @@
     self.separatorLineWidth = PixelToPoint(1.0f) ;
     [self.collectionView reloadData] ;
     //图片名称数组
-    _picNameArray = @[@"101",@"302",@"504",@"1006",@"4998"];
+    _picNameArray = @[@"100",@"200",@"500",@"1000",@"5000"];
     [self setupUI];
 }
 
@@ -94,19 +94,19 @@
 {
     ifRespondsSelector(self.delegate, @selector(depositeChooseMoneyCell:)){
         if (indexPath.item==0) {
-            [self.delegate depositeChooseMoneyCell:101] ;
+            [self.delegate depositeChooseMoneyCell:100] ;
         }
         else if (indexPath.item==1) {
-            [self.delegate depositeChooseMoneyCell:302] ;
+            [self.delegate depositeChooseMoneyCell:200] ;
         }
         else if (indexPath.item==2) {
-            [self.delegate depositeChooseMoneyCell:504] ;
+            [self.delegate depositeChooseMoneyCell:500] ;
         }
        else if (indexPath.item==3) {
-            [self.delegate depositeChooseMoneyCell:1006] ;
+            [self.delegate depositeChooseMoneyCell:1000] ;
         }
        else if (indexPath.item==4) {
-            [self.delegate depositeChooseMoneyCell:4998] ;
+            [self.delegate depositeChooseMoneyCell:5000] ;
         }
     }
 }
