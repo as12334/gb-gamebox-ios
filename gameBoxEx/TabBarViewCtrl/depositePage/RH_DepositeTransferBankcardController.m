1d5395e83b3edd849cf6d1e1589f99fe3be6c739
shin <shin@dawoo.com>
2018-06-05 15:26:00 +0800

    订单号填写的处理

--- gameBoxEx/TabBarViewCtrl/depositePage/RH_DepositeTransferBankcardController.m
+++ gameBoxEx/TabBarViewCtrl/depositePage/RH_DepositeTransferBankcardController.m
@@ -630,7 +630,7 @@
     else if ([self.accountMuArray[2] isEqualToString:@"onecodepay"])
     {
         if (self.paywayCell.paywayString.length!=5&&self.paywayCell.paywayString.length!=0&&self.paywayCell.paywayString!=nil) {
-            showMessage(self.view, @"请输入只含数字、字母的订单号", nil);
+            showMessage(self.view, @"请输入五位只含数字、字母的订单号", nil);
         }
         else if(self.paywayCell.paywayString.length!=0){
             NSString *regex = @"[A-Za-z0-9]*";
