9d48f1ce91af8a1e290d9b820efa2c9281cf18ff
jun <jun@dawoo.com>
2018-07-02 20:25:23 +0800

    changed

--- gameBoxEx/OtherViewCtrl/loginViewCtrl/RH_LoginViewControllerEx.m
+++ gameBoxEx/OtherViewCtrl/loginViewCtrl/RH_LoginViewControllerEx.m
@@ -414,7 +414,12 @@
             [self.delegate loginViewViewControllerExTouchBack:self BackToFirstPage:_backToFirstPage];
         }
     }else{
-        [self.navigationController popToRootViewControllerAnimated:NO];
+        if (self.presentingViewController) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }else{
+             [self.navigationController popToRootViewControllerAnimated:NO];
+        }
+       
         self.myTabBarController.selectedIndex = 0 ;
     }
     
