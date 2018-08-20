26be318e429fde985ba45012ce0cd5c32cc65ff7
sam <git@git.gbboss.com:gb/gamebox-ios-chess.git>
2018-08-20 16:19:02 +0800

    线路提示更改

--- SplashViewController/StartPageViewController.m
+++ SplashViewController/StartPageViewController.m
@@ -711,8 +711,19 @@
     NSDictionary *infoDic = [[NSBundle mainBundle] infoDictionary];
     NSString *appVersion = [infoDic objectForKey:@"CFBundleShortVersionString"];
     NSString *ip = [self localIPAddress];
-    NSString *msg = [NSString stringWithFormat:@"\n错误码:%@\n当前ip:%@\n版本号:%@\n很抱歉,请联系客服并提供以上信息",code, ip, [NSString stringWithFormat:@"iOS %@.%@",appVersion,RH_APP_VERCODE]];
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"线路检测失败" message:msg preferredStyle:UIAlertControllerStyleAlert];
+    NSString *title;
+    NSString *msg ;
+    if ([code isEqualToString:@"001"]) {
+        title = @"网络连接失败";
+        msg = [NSString stringWithFormat:@"\n网络连接失败，请确认您的网络连接正常后再次尝试！"];
+    } else if ([code isEqualToString:@"002"]) {
+        title = @"线路获取失败";
+        msg = [NSString stringWithFormat:@"\n线路获取失败，请确认您的网络连接正常后再次尝试！"];
+    } else if ([code isEqualToString:@"003"]) {
+        title = @"服务器连接失败";
+        msg = [NSString stringWithFormat:@"\n当前ip:%@\n版本号:%@\n出现未知错误，请联系在线客服并提供以上信息。", ip, [NSString stringWithFormat:@"iOS %@.%@",appVersion,RH_APP_VERCODE]];
+    }
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:msg preferredStyle:UIAlertControllerStyleAlert];
     
     NSMutableAttributedString *messageText = [[NSMutableAttributedString alloc] initWithString:msg];
     NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
