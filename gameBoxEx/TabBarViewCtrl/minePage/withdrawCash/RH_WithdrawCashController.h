28b3723720d6fb3941dc91fe2ae01bb58034193a
jun <jun@dawoo.com>
2018-09-20 15:00:07 +0800

    修改提现页面导航栏

--- gameBoxEx/TabBarViewCtrl/minePage/withdrawCash/RH_WithdrawCashController.h
+++ gameBoxEx/TabBarViewCtrl/minePage/withdrawCash/RH_WithdrawCashController.h
@@ -6,8 +6,8 @@
 //  Copyright © 2018年 luis. All rights reserved.
 //
 
-#import "RH_BasicPageLoadViewController.h"
+#import "RH_BasicSubViewController.h"
 
-@interface RH_WithdrawCashController : RH_BasicPageLoadViewController
+@interface RH_WithdrawCashController : RH_BasicSubViewController
 
 @end
